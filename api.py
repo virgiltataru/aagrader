65c4669338c896dab51d2eeb9d51a7f5889e86bb
Muddassar Sharif <muddassarsharif@Muddassars-MacBook-Pro.local>
2018-11-08 00:38:19 +0800

    API Done

--- api.py
+++ api.py
@@ -1,39 +1,51 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from requests import put, get
+from Classes import Status
+from runner import run
+from test_data import tc1, python3_source_code_add_two_numbers, tc2
+import flask
+import json
+from Classes import Testcase
 
 app= Flask(__name__)
 api= Api(app)
 
 class check_code(Resource):
     def get(self):
-        # Use if required
-        return {'about':'hello_world'}
+        return {'about':'Hi! Do you want me to check your code?'}
 
     def post(self):
-        # { problem_id: __ , 'test_case': ___ , 'submitted_code': ___ }
+
+        # if input from JSON
         json_data = request.get_json(force=True)
 
-        # each problem has a problem ID. This might be useful. Discard if not required
-        problem_id = json_data['problem_id']
-
-        # 'test_case': {'id':___ , 'input':____ , 'file'____ }
-        test_case = json_data['test_case']
-
-        # 'submitted_code': {'source':__, 'programming_language': 'python'}
-        code_submitted= json_data['submitted_code']
-
-        # Call all the classes to check the code_sibmitted
+        python3_code= json_data['submitted_code']
+        test_input= json_data['test_input']
+        test_output= json_data['test_output']
+        extension= json_data['extension']
 
 
+        tc1 = Testcase()
+        tc1.id = "1"
+        tc1.input = test_input
+        tc1.timeout = 1
 
+        out = run(python3_code, extension , None, ["python3", "Source.py"], [tc1])
 
-        # to send the output back to the other webservice.
-            # put('http://localhost:5000/', data={'data': 'this is the data'}).json()
-        put('', data={'data': 'Congratulations! this is done.'}).json()
+        print(out)
+        try:
+            assert 1 != len(out)
+            return {'message':'Something got wrong', 'result': str(out), 'pass':-1}, 201
+        except:
+            pass
+        try:
+            assert str(test_output) == out[0].stdout
+            assert Status.OK == out[0].status
+            return {'result':str(out), 'remarks': 'Code submitted is correct!', 'pass':1}, 201
+        except:
+            return {'result':str(out), 'remarks': 'Code submitted is incorrect', 'pass':0}, 201
 
-        # return function might also do the job
-        return {'you_sent':some_json}, 201
 
 
 api.add_resource(check_code,'/')
