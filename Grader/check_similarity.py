a7e4e873531eddbe793dc4b71806812c805183fc
virgiltataru <virgiltataru97@gmail.com>
2018-12-01 14:17:37 +0800

    documentation

--- Grader/check_similarity.py
+++ Grader/check_similarity.py
@@ -4,17 +4,21 @@ import mosspy
 
 MOSS_USERID = 535989500
 
+#function to check similarity of all the submissions of a specific hw
+#@return a link to a MOSS querry
+
 def check_similarity(hw_id):
+    #create a new directory to store all the files
     current_directory = os.getcwd()
     temp_dir = uuid.uuid4().hex
     os.makedirs(temp_dir)
     os.chdir(temp_dir)
-
+    #populate files with student names and subbmited code
     ans= mongo.db.submissions.find({"hw_id": hw_id})
     for a in ans:
         with open(a['Student'] + ".py", 'w+') as f:
             f.write(a['code'])
-
+    #send a MOSS querry and delete the folder with the files afterwards
     m = mosspy.Moss(MOSS_USERID, "python")
     m.addFilesByWildcard("*.py")
     url = m.send()
